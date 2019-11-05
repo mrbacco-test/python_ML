e36112c67bf091d3f5c59e0bb6def0ec1f42ec82
mrbacco2 <mrbacco@outlook.com>
2019-11-05 15:33:51 +0000

    commit

---
+++ data.py
@@ -0,0 +1,22 @@
+def Items():
+    items = [
+        {
+            'id': 1,
+            'name': 'andrea',
+            'title': 'product developer',
+            'description': 'managing and developing products stuff',
+        },
+        {
+            'id': 2,
+            'name': 'luke',
+            'title': 'evangelist',
+            'description': 'distributing the knowledge'
+        },
+        {
+            'id': 3,
+            'name': 'james',
+            'title': 'writer',
+            'description': 'writing about the stuff created'
+        }
+    ]
+    return items
